e6a69931fec4689ea323446484a872f597c00b83
Michael Lee <michael@mrlee.io>
2023-10-20 10:56:07 -0600

    Bullet
    https://github.com/flyerhzm/bullet
    
    The Bullet gem is designed to help you increase your application's
    performance by reducing the number of queries it makes. It will watch
    your queries while you develop your application and notify you when you
    should add eager loading (N+1 queries), when you're using eager loading
    that isn't necessary and when you should use counter cache.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Bullet - https://github.com/flyerhzm/bullet
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,4 +63,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Bullet - https://github.com/flyerhzm/bullet
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.raise         = true
+  end
 end
